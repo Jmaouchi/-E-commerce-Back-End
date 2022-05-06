e4abb38e14f51366f3a03f19f0e3eeb9280966a3
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-06 15:27:52 -0500

    added api routes to get all collumns and one collumn from the tag model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,4 +3,59 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+router.get('/', (req, res) => {
+  // find all categories
+  Tag.findAll({
+    attributes: [
+      'id', 'tag_name'
+    ],
+    // include the product table data. 
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ]
+  })
+  // then send a response as json
+.then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No Tags found' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  Tag.findOne( {
+    where:{
+      id:req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ]
+  })
+  // then send a response as json
+.then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No Tags found' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
