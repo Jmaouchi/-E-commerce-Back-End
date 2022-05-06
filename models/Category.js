366c1ada41ca57c693ba64329991373bfc21f99a
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-05 23:21:42 -0500

    Created Tables, and added the communication between these tables

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,23 @@
+// import sequelize model from library
 const { Model, DataTypes } = require('sequelize');
-
+// import the connection to the db 
 const sequelize = require('../config/connection.js');
-
+// create Category Model
 class Category extends Model {}
 
-Category.init(
+Category.init( 
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    }
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,6 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
@@ -14,11 +12,11 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
