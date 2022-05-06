94df866b33a988a2a5c1c795aed94c70fc4fa486
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-05 23:22:26 -0500

    added api route to get all the data from the Category model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,6 +4,27 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  // find all categories
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    // include the product table data. 
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      },
+    ]
+  })
+  // then send a response as json
+  .then(data => res.json(data))
+  // catch if there is any errors caused by the server, if so send a 500 server error 
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
