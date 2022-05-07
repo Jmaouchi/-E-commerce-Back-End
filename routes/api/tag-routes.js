b4f43bb84ef7e3da5cb21c64f88fc671ea01d2c9
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-05-06 21:32:19 -0500

    added api route to get all tags and a singe tah from Tag table

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,12 +6,59 @@ const { Tag, Product, ProductTag } = require('../../models');
 // get all products from the product model and include the category name
 router.get('/', (req, res) => {
   // find all categories
+  Tag.findAll({
+    attributes: [
+      'id', 'tag_name'
+    ],
+    // include the product table data. 
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ]
+  })
+  // then send a response as json
+.then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No Tags found' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 
 // get one tag from the Tag model and include all collumns from Product model
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
+  Tag.findOne( {
+    where:{
+      id:req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      },
+    ]
+  })
+  // then send a response as json
+.then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No Tags found' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 
@@ -28,7 +75,7 @@ router.put('/:id', (req, res) => {
 
 
 
-// // DELETE an  existing tag in the Tag model
+// DELETE an existing tag in the Tag model
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
 });
